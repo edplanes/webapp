265173631bc18aa55e0e0ef737b3ec3eaa0ecd2b
Kacper Malachowski <kacpermaachowski@gmail.com>
2023-12-23 21:02:39 +0100

    Remove unused component

--- src/app/pages/user/user-home/user-home.component.ts
+++ src/app/pages/user/user-home/user-home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { FlightCardComponent } from '../../../components/flight-card/flight-card.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [FlightCardComponent, FlexLayoutModule],
+  imports: [FlexLayoutModule],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.scss',
 })
